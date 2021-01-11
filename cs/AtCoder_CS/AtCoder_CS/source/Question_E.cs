f9cc1f2d45d3784fe15237c98b3f6ff705ec642a
tngrcorn <tknk.nykk@gmail.com>
2021-01-11 17:40:29 +0900

    C#用プロジェクト作成

---
+++ cs/AtCoder_CS/AtCoder_CS/source/Question_E.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AtCoder_CS.source
+{
+    class Question_E
+    {
+        public static void Main(string[] args)
+        {
+            var sw = new System.IO.StreamWriter(Console.OpenStandardOutput()) { AutoFlush = false };
+            Console.SetOut(sw);
+
+            // 文字列の入力
+            string s = Console.ReadLine();
+
+            // 整数の入力
+            long n = long.Parse(Console.ReadLine());
+
+            // 文字列配列の入力
+            string[] inputStrArray = Console.ReadLine().Split(' ');
+
+            // 整数配列の入力
+            var inputLongArray = Console.ReadLine().Split(' ').Select(i => long.Parse(i)).ToArray();
+
+
+            Console.WriteLine(s);
+
+            Console.Out.Flush();
+        }
+    }
+}
